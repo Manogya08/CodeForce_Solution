b4c1f380feeb5df6a59b8eb37f630f75326ff160
Manogya Rastogi <43292289+Manogya08@users.noreply.github.com>
2021-07-31 09:24:24 +0530

    Add files via upload
    AquaMoon has n friends. They stand in a row from left to right, and the i-th friend from the left wears a T-shirt with a number ai written on it. Each friend has a direction (left or right). In the beginning, the direction of each friend is right.
    
    AquaMoon can make some operations on friends. On each operation, AquaMoon can choose two adjacent friends and swap their positions. After each operation, the direction of both chosen friends will also be flipped: left to right and vice versa.
    
    AquaMoon hopes that after some operations, the numbers written on the T-shirt of n friends in the row, read from left to right, become non-decreasing. Also she wants, that all friends will have a direction of right at the end. Please find if it is possible.
    
    Input
    The input consists of multiple test cases. The first line contains a single integer t (1≤t≤50) — the number of test cases.
    
    The first line of each test case contains a single integer n (1≤n≤105) — the number of Aquamoon's friends.
    
    The second line contains n integers a1,a2,…,an (1≤ai≤105) — the numbers, written on the T-shirts.
    
    It is guaranteed that the sum of n for all test cases does not exceed 105.
    
    Output
    For each test case, if there exists a possible sequence of operations, print "YES" (without quotes); otherwise, print "NO" (without quotes).
    
    You can print each letter in any case (upper or lower).

---
+++ A. AquaMoon and Strange Sort.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, a[100005];
+int main() {
+
+	int t;
+	cin >> t;
+	while(t--) {
+		cin >> n;
+		vector<int> v1, v2;
+		for(int i = 1; i <= n; i++) {
+			cin >> a[i];
+			if(i & 1) v1.push_back(a[i]);
+			else v2.push_back(a[i]);
+		}
+		sort(v1.begin(), v1.end());
+		sort(v2.begin(), v2.end());
+		int cnt1 = 0, cnt2 = 0;
+		vector<int> arr;
+		for(int i = 1; i <= n; i++) {
+			if(i & 1) arr.push_back(v1[cnt1++]);
+			else arr.push_back(v2[cnt2++]);
+		}
+		bool flag = 1;
+		for(int i = 1; i < arr.size(); i++) {
+			if(arr[i] < arr[i - 1]) {
+				flag = 0;
+				break;
+			}
+		}
+		if(flag) cout << "YES" << endl;
+		else cout << "NO" << endl;
+	}
+}
